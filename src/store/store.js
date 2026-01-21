f0d6e5087f1ff2b1577cfa6dca95253943838e90
vinuths <vinuthgowda85@gmail.com>
2026-01-21 09:30:57 +0530

    elibrary

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,10 @@
 import { createStore, combineReducers, applyMiddleware/*,compose*/ } from 'redux';  // configureStore will be used to create store. combineReducers will be used for combining all reducers, applyMiddlware will be used with redux-thunk to apply thunk middlewares if needs.
-import thunk from 'redux-thunk';  // if after getting dispatch from action if  it returns function thunk will apply ascynchronously some logic as middleware then give it to reducer.
+import { thunk } from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-// import {
-//         userLoginReducer,
-// } from './reducers/authReducers';  // imporeting auth user reducers
+import {
+        userLoginReducer,
+} from './reducers/authReducers';  // imporeting auth user reducers
 
 import {
         auditReducer,
@@ -200,7 +200,7 @@ const reducer = combineReducers({
         filterAllAudit: auditAllFilterReducer,
         onGoingAudit: auditOnGoingReducer,
         overDueAudit: auditOverDueReducer,
-        // userLogin: userLoginReducer,
+        userLogin: userLoginReducer,
         catCreate: categoryReducer,
         catGet: categoryGetReducer,
         catEdit: categoryEditReducer,
@@ -384,7 +384,7 @@ const initialState = {
 
 const middleware = [thunk];
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)));//this is also correct
+const store = createStore(reducer,  composeWithDevTools(applyMiddleware(...middleware)));//this is also correct
 //const store = createStore(reducer,initialState,composeWithDevTools(applyMiddleware(thunk))); // this is also correct
 
 export default store;
